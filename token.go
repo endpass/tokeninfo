27605a176e0e0dc94dea79af5b1e51ca5a5826f2
Dutch <dutch@unviral.com>
2018-04-23 17:04:59 -0400

    Parse token list JSON

--- token.go
+++ token.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+)
+
 // Info about a single ERC20 token
 type Token struct {
 	// Long name of the token
@@ -9,7 +14,40 @@ type Token struct {
 	// Address of the token contract
 	Address string `json:"address"`
 	// Number of decimal points in amounts
-	Decimals uint8 `json:"decimals"`
+	Decimals int `json:"decimals"`
 	// Path to logo image
 	Logo string `json:"logo"`
+}
+
+// Token info from
+// https://github.com/MyEtherWallet/ethereum-lists/blob/master/tokens/tokens-eth.json
+type tokenListToken struct {
+	// Long name of the token
+	Name string `json:"name"`
+	// Symbol on exchanges
+	Symbol string `json:"symbol"`
+	// Address of the token contract
+	Address string `json:"address"`
+	// Number of decimal points in amounts
+	Decimals int `json:"decimals"`
+	// Path to logo image
+	Logo string `json:"-"`
+}
+
+func ParseTokenList(r io.Reader) ([]*Token, error) {
+	var tokens []*tokenListToken
+	decoder := json.NewDecoder(r)
+	if err := decoder.Decode(&tokens); err != nil {
+		return nil, err
+	}
+	results := make([]*Token, len(tokens))
+	for i, token := range tokens {
+		results[i] = &Token{
+			Name:     token.Name,
+			Symbol:   token.Symbol,
+			Address:  token.Address,
+			Decimals: token.Decimals,
+		}
+	}
+	return results, nil
 }
---
+++ token_test.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testList = `
+[
+	{
+		"symbol": "GIF",
+		"address": "0xFcD862985628b254061F7A918035B80340D045d3",
+		"decimals": 18,
+		"name": "GIFcoin Token",
+		"ens_address": "",
+		"website": "https://gifcoin.io/",
+		"logo": {
+			"src":
+				"https://www.gifcoin.io/assets/images/default/frontend/gifsingle400.png",
+			"width": "400",
+			"height": "400",
+			"ipfs_hash": ""
+		},
+		"support": {
+			"email": "support@gifcoin.io",
+			"url": ""
+		},
+		"social": {
+			"blog": "",
+			"chat": "",
+			"facebook": "https://facebook.com/gifcoin.io",
+			"forum": "",
+			"github": "",
+			"gitter": "",
+			"instagram": "",
+			"linkedin": "",
+			"reddit": "",
+			"slack": "",
+			"telegram": "https://t.me/gifcoin",
+			"twitter": "https://twitter.com/gifcoin_io",
+			"youtube": "https://youtube.com/channel/UCLq13wzOH1STqW8I-Z-ctAQ"
+		}
+	},
+	{
+		"symbol": "DTT",
+		"address": "0xf9F7c29CFdf19FCf1f2AA6B84aA367Bcf1bD1676",
+		"decimals": 18,
+		"name": "Delphi Tech Token",
+		"ens_address": "",
+		"website": "https://delphifund.org/",
+		"logo": {
+			"src":
+				"https://delphifund.org/wp-content/uploads/2018/04/delphi400x400.jpg",
+			"width": "400",
+			"height": "400",
+			"ipfs_hash": ""
+		},
+		"support": {
+			"email": "support@delphifund.org",
+			"url": ""
+		},
+		"social": {
+			"blog": "",
+			"chat": "",
+			"facebook": "",
+			"forum": "",
+			"github": "https://github.com/DTToken",
+			"gitter": "",
+			"instagram": "",
+			"linkedin": "",
+			"reddit": "",
+			"slack": "",
+			"telegram": "",
+			"twitter": "https://twitter.com/Delphitechtoken",
+			"youtube": ""
+		}
+	}
+]
+`
+
+func TestTokenList(t *testing.T) {
+	assert := assert.New(t)
+	r := strings.NewReader(testList)
+	tokens, err := ParseTokenList(r)
+	assert.NoError(err)
+	assert.Len(tokens, 2)
+	assert.Equal("GIFcoin Token", tokens[0].Name)
+	assert.Equal("0xFcD862985628b254061F7A918035B80340D045d3", tokens[0].Address)
+	assert.Equal("GIF", tokens[0].Symbol)
+	assert.Equal(18, tokens[0].Decimals)
+	assert.Equal(tokens[1].Symbol, "DTT")
+}
