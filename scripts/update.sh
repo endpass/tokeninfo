5d98d8b96caf0610d90f355369ef175c0c29f26d
Dutch <dutch@unviral.com>
2018-05-07 16:38:34 -0400

    Fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,7 +5,7 @@
 [ -z "$TOKEN_LIST" ] && echo "TOKEN_LIST is not set" && exit 1
 [ -z "$TOKEN_IMAGE_DIR" ] && echo "TOKEN_IMAGE_DIR is not set" && exit 1
 
-cd "${TOKEN_LIST}" && git pull
+cd "$(dirname ${TOKEN_LIST})" && git pull
 cd "${TOKEN_IMAGE_DIR}" && git pull
 
 exit
